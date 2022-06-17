b48d57a69953e16fb63a9baa1dd147055665f60f
benjaminjacober <benjamin.jacober@gmail.com>
2022-06-17 14:41:23 +0200

    asdlfkjasd

--- praxisarbeit/bin/backup.sh
+++ praxisarbeit/bin/backup.sh
@@ -7,8 +7,16 @@ while getopts c:o:g:e flag; do
   esac
 done
 
-DIR="$pwd"
+DIR="/home/benjaminjacober/Documents/GitHub/tbzm122/praxisarbeit/backups"
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
-  echo "Installing config files in ${DIR}..."
+  echo $DIR
+  for userDir in $DIR; do
+    cd "$DIR$userDir"
+    echo $userDir
+    echo ls | wc -l
+  done
+
+else
+  echo "donf"
 fi
