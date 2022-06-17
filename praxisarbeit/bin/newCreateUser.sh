a77feb80d2ffdb17a3c28ef5b63ec1a5a0c04d8d
benjaminjacober <benjamin.jacober@gmail.com>
2022-06-17 13:54:38 +0200

    asdlfkjasd

---
+++ praxisarbeit/bin/newCreateUser.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Reading input parameters
+while getopts i:p:c: flag; do
+  case "${flag}" in
+  i) i=${OPTARG} ;;
+  p) p=${OPTARG} ;;
+  c) c=${OPTARG} ;;
+  esac
+done
+
+echo "inputFile: $i"
+echo "password: $p"
+echo "createGroup: $c"
+
+# Reading File
+a=1
+while read ln; do
+  # Get names
+  lineArray=($ln)
+  user=${lineArray[0]}
+  group=${lineArray[1]}
+  firstName=${lineArray[2]}
+  lastName=${lineArray[3]}
+
+  # Check if group exists
+  if grep -q $group /etc/group; then
+    echo "group exists"
+    sudo usermod -a -G $group $user
+  else
+    echo "group does not exist"
+    if [ $c ]; then
+          sudo groupadd $group
+          sudo usermod -a -G $group $user
+    else
+      echo "There is no group $c and createGroup flag is not set. No user was created"
+    fi
+  fi
+
+  a=$((a + 1))
+done <$i
+
+echo "$fileArray"
