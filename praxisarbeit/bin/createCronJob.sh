cec29100c163b4f6dcd381093675cb6c76cc642c
benjaminjacober <jacober.benjamin@gmail.com>
2022-06-22 16:41:12 +0200

    asdf

--- praxisarbeit/bin/createCronJob.sh
+++ praxisarbeit/bin/createCronJob.sh
@@ -13,10 +13,11 @@ cat ../etc/createCronJob.conf | grep -v '^#' | grep -v '^$' | while read group; 
   if grep -q $group /etc/group; then
     echo "Creating cron job"
     #echo new cron into cron file
-    echo "00 00 * 00 00 $command" >> mycron
+    echo "00 00 * 00 00 $command" >>mycron
     #install new cron file
     crontab mycron
     rm mycron
   else
     echo "Group does not exist, did not set up cron job"
+  fi
 done
